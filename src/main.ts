cb73c0ef739e3c92e03aba5a6edc2af910eca88d
IlyaSharafeev <ilyasharafeev01@gmail.com>
2025-06-19 21:51:00 +0300

    WIP | connect i18n

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia';
 import "vue-multiselect/dist/vue-multiselect.min.css";
 import Notifications from '@kyvg/vue3-notification'
-import i18n from '@/locales/vue-i18n';
+import i18n from '@/locales/vue-i18n.js';
 
 const pinia = createPinia();
 const app = createApp(App);
