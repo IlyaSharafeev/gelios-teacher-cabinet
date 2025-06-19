a2a92d71754a0c0aeb3c110c3a33e992f71eddf1
IlyaSharafeev <ilyasharafeev01@gmail.com>
2025-06-19 21:28:45 +0300

    WIP | connect i18n

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path';
 
 export default defineConfig({
-  plugins: [vue(), vueI18n({
-    include: path.resolve(__dirname, './src/locales/languages/**')
-  })],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
