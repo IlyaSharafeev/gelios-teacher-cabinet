cc66128617e80aacf3b527a06ce35ffa7c21108f
IlyaSharafeev <ilyasharafeev01@gmail.com>
2025-07-14 17:32:25 +0300

    WIP | Sass migrator, fix warnings

--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-<h1>Shedule</h1>
+  <div class="calendar-wrapper">
+    <iframe
+        src="https://outlook.live.com/owa/calendar/ad6f9905-4e27-4088-a718-00b7e638b9c2/e7b0f848-640f-45fb-bb1a-e3512ada07ce/cid-C1863F8E504EC047/index.html"
+        style="width: 100%; height: 100vh; border: none"
+    ></iframe>
+  </div>
 </template>
 
 <style scoped>
