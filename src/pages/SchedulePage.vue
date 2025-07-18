36a44cbb9075430447a49631b7c60b3f2b07d737
IlyaSharafeev <ilyasharafeev01@gmail.com>
2025-07-18 18:19:56 +0300

    WIP | import calendar

--- src/pages/SchedulePage.vue
+++ src/pages/SchedulePage.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="calendar-wrapper">
     <iframe
-        src="https://outlook.live.com/owa/calendar/ad6f9905-4e27-4088-a718-00b7e638b9c2/cf441ea5-3d70-4e9b-b9bd-5122b7c6015e/cid-C1863F8E504EC047/index.html"
+        src="https://gelios-calendar-lib.netlify.app/"
         style="width: 100%; height: 100vh; border: none"
     ></iframe>
   </div>
